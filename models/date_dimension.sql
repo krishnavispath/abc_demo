9671db40eb40470553912fc5d7b4c0971f942c10
m krishna <krishnavispath@gmail.com>
2025-12-28 11:05:08 +0000

    date_dimension and date_utils done

---
+++ models/date_dimension.sql
@@ -0,0 +1,16 @@
+WITH CTE AS(
+SELECT 
+to_timestamp(started_at) as started_at,
+date(to_timestamp(started_at)) as date_started_at,
+hour(to_timestamp(started_at)) as hour_started_at,
+
+{{day_type('started_at')}} as day_type,
+
+{{get_season('started_at')}} as Station_of_year
+
+FROM
+{{source("RAW",'BIKE_RIDES')}}
+
+)
+
+SELECT * FROM CTE
